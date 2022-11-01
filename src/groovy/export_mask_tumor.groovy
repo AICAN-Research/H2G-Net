d918f00b08ca24d95d64bd0b8f1a4b47dd38f997
André Pedersen <andrped94@gmail.com>
2022-11-01 14:03:34 +0100

    refactored groovy code

--- src/groovy/export_mask_tumor.groovy
+++ src/groovy/export_mask_tumor.groovy
@@ -15,32 +15,17 @@ import qupath.lib.io.PathIO
 import qupath.lib.scripting.QPEx // <- works for < Java 11 (pre QuPath-0.2.0)
 //import qupath.lib.gui.scripting.QPEx // <- for Java 11 (QuPath-0.2.0)
 
-//--- test ---
-
 def qupath = QPEx.getQuPath()
-
-//print qupath
-//print ' 
-
-// collect image data
-//String img_loc = '/home/andre/Documents/DP/AI_Marit_valla/137.vsi'
-//String loc = '/home/andre/Documents/DP/Qupath_gt'
-//PathIO.readImageData(img_loc)
-//String proj_loc = '/home/andre/Documents/DP/Qupath_gt/project_test.qpproj'
 
 // set downsampling level
 double downsample = 4
 
 // Output directory for storing the tiles
 def pathOutput = QPEx.buildFilePath(QPEx.PROJECT_BASE_DIR, 'exported_mask_png' + "_" + ((int)downsample).toString())
-//String loc = '/home/andre/Documents/DP/Qupath_gt'
-//def pathOutput = QPEx.buildFilePath(loc, 'exported_mask')
 QPEx.mkdirs(pathOutput)
 
 // Get the main QuPath data structures
 def imageData = QPEx.getCurrentImageData()
-
-//print imageData
 
 def hierarchy = imageData.getHierarchy()
 def servers = imageData.getServer()
@@ -48,22 +33,12 @@ def servers = imageData.getServer()
 String path = servers.getPath()
 String name = path.tokenize('/')[-1]
 String names = name.tokenize('.')[-2]
-//print 11111111111
-print names
 
 // only extract tumor class
 def tumor = getPathClass('Tumor')
-//print 2222222222
-//print '\n' + tumor + '\n'
 def annotations = getAnnotationObjects()
 def tumorAnnotations = annotations.findAll {it.getPathClass() == tumor}
 def shapes = tumorAnnotations.collect {getShape(it.getROI())}
-
-//print '\n' + annotations
-//print ' '
-
-// Get java.awt.Shape objects for each annotation
-// def shapes = getAnnotationObjects().collect({getShape(it.getROI())})
 
 // Create a grayscale image, here it's 10% of the full image size
 def server = getCurrentImageData().getServer()
@@ -80,11 +55,6 @@ for (shape in shapes)
 g2d.dispose()
 
 // Save the result -> save mask as PNG
-//print name
-print pathOutput
-print names + '.tif'
-print img
 def outputFile = new File(pathOutput, names + '.png') //getQuPath().getDialogHelper().promptToSaveFile("Save binary image", null, null, "PNG", ".png")
 ImageIO.write(img, 'PNG', outputFile)
 print 'Done!'
-
